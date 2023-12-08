d491e2e929a3cfb1ea08662ee93c400b5a975714
alexndra1272 <alexandrag1272@gmail.com>
2023-12-08 15:30:31 -0600

    Creacion de la estructura del front

---
+++ Frontend/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Frontend.Views;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Frontend
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void fnMinimizar(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void fnCerrar(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void PersonaView(object sender, RoutedEventArgs e)
+        {
+            DataContext = new Persona();
+        }
+    }
+}
