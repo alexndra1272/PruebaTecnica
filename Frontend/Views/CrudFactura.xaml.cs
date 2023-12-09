b358e5b9bf1cb30603c1abdadc4929a52b35c3b7
alexndra1272 <alexandrag1272@gmail.com>
2023-12-08 23:44:14 -0600

    Feat: Mostrar personas en fact

--- Frontend/Views/CrudFactura.xaml.cs
+++ Frontend/Views/CrudFactura.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Frontend.models;
+using System.Net.Http;
+using System.Net.Http.Headers;
 
 namespace Frontend.Views
 {
@@ -20,11 +23,42 @@ namespace Frontend.Views
     /// </summary>
     public partial class CrudFactura : Page
     {
+        private readonly HttpClient client = new HttpClient();
+
         public CrudFactura()
         {
             InitializeComponent();
 
+            client.BaseAddress = new Uri("https://localhost:7277/api/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json")
+            );
+
+            // Registra el evento Loaded
+            Loaded += CrudPersona_Loaded
+
         }
+        private void CrudPersona_Loaded(object sender, RoutedEventArgs e)
+        {
+            // cargar personas
+            cargarPersonas();
+        }
+
+        private void cargarPersonas()
+        {
+            // Llamada a la API
+            HttpResponseMessage response = client.GetAsync("persona").Result;
+            // Verificar si la respuesta es exitosa
+            if (response.IsSuccessStatusCode)
+            {
+                // Obtener los datos del API y asignarlos a la propiedad Items
+                var personas = response.Content.ReadAsAsync<IEnumerable<Personas>>().Result;
+                // Asignar los datos en el DataGrid
+                CboxPersona.ItemsSource = personas;
+            }
+        }
+
         private void BtnCancelar_Click(object sender, RoutedEventArgs e)
         {
             Content = new Factura();
