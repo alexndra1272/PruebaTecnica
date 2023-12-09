76489abb000c1ce2e0a72f49d2f6362349499055
alexndra1272 <alexandrag1272@gmail.com>
2023-12-08 18:52:34 -0600

    Fix: Actualizacion de los modelos

--- Backend/Data/Models/Persona.cs
+++ Backend/Data/Models/Persona.cs
@@ -3,10 +3,18 @@ namespace Backend.Data.Models
 {
     public class Persona
     {
+        [ Required ]
         public int IdPersona { get; set; }
+        [ Required ]
         public string Nombre { get; set; }
+        [ Required ]
         public string ApellidPaterno { get; set; }
+
         public string? ApellidMaterno { get; set; } = null!; //optional
+
+        [ Required ]
+        [ MaxLength(15) ]
+        [ Index(IsUnique = true) ]
         public string Identificacion { get; set; }
 
 
