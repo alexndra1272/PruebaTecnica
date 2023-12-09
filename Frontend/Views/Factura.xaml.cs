ef2f010a5efdf49618df6aa1440775540241a76a
alexndra1272 <alexandrag1272@gmail.com>
2023-12-08 22:12:53 -0600

    Feat: Mostrar facturas

--- Frontend/Views/Factura.xaml.cs
+++ Frontend/Views/Factura.xaml.cs
@@ -12,6 +12,11 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Frontend.models;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+
 
 namespace Frontend.Views
 {
@@ -20,11 +25,37 @@ namespace Frontend.Views
     /// </summary>
     public partial class Factura : UserControl
     {
+        private readonly HttpClient client = new HttpClient();
         public Factura()
         {
+            client.BaseAddress = new Uri("https://localhost:7277/api/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json")
+            );
+            // Registra el evento Loaded
+            Loaded += Factura_Loaded;
+            
             InitializeComponent();
         }
-
+        private void Factura_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Llamada al método de carga después de que el control se ha cargado
+            cargarFacturas();
+        }
+        private void cargarFacturas()
+        {
+            // Llamada a la API
+            HttpResponseMessage response = client.GetAsync("factura").Result;
+            // Verificar si la respuesta es exitosa
+            if (response.IsSuccessStatusCode)
+            {
+                // Obtener los datos del API y asignarlos a la propiedad Items
+                var facturas = response.Content.ReadAsAsync<IEnumerable<Facturas>>().Result;
+                // Asignar los datos en el DataGrid
+                DatosFactura.ItemsSource = facturas;
+            }
+        }
         private void TxtBuscarFact_TextChanged(object sender, TextChangedEventArgs e)
         {
 
---
+++ Frontend/models/Facturas.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frontend.models
+{
+    public class Facturas
+    {
+        public int IdFactura { get; set; }
+        public DateTime Fecha { get; set; }
+        public decimal Monto { get; set; }
+        public int IdPersona { get; set; }
+        
+    }
+}
